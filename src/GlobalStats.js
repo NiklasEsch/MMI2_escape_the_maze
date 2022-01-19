27f9ef83cb8d2f335f4ee76aeef29874d1a107ad
Fangli Lu <fangli.lu@outlook.de>
2022-01-19 15:36:04 +0100

    add collect coin functionality, animations, feeding restriction

--- src/GlobalStats.js
+++ src/GlobalStats.js
@@ -31,4 +31,88 @@ const MonsterPositions = [
             z: -55.6    
         }
     }
-]
+];
+
+const CoinsPositions = [
+    {
+        name: 'coin-0',
+        pos: {
+            x: -38,
+            y: 0,
+            z: -10
+        }
+    },
+    {
+        name: 'coin-1',
+        pos: {
+            x: -21,
+            y: 0,
+            z: -12
+        }
+    },
+    {
+        name: 'coin-2',
+        pos: {
+            x: -21,
+            y: 0,
+            z: -33
+        }
+    },
+    {
+        name: 'coin-3',
+        pos: {
+            x: -32,
+            y: 0,
+            z: -33
+        }
+    },
+    {
+        name: 'coin-4',
+        pos: {
+            x: -38,
+            y: 0,
+            z: -55
+        }
+    },
+    {
+        name: 'coin-5',
+        pos: {
+            x: -28,
+            y: 0,
+            z: 52
+        }
+    },
+    {
+        name: 'coin-6',
+        pos: {
+            x: -37,
+            y: 0,
+            z: 19
+        }
+    },
+    {
+        name: 'coin-7',
+        pos: {
+            x: -50,
+            y: 0,
+            z: 29
+        }
+    },
+    {
+        name: 'coin-8',
+        pos: {
+            x: -44,
+            y: 0,
+            z: 41
+        }
+    },
+    {
+        name: 'coin-9',
+        pos: {
+            x: -38,
+            y: 0,
+            z: 52
+        }
+    },
+
+];
