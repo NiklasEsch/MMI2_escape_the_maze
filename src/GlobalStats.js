4d7c9902d0562d6057d01d2038094e031f708594
Fangli Lu <fangli.lu@outlook.de>
2022-01-19 11:02:08 +0100

    add feeding cat when near and remove it afterwards

---
+++ src/GlobalStats.js
@@ -0,0 +1,34 @@
+const MonsterPositions = [
+    {
+        name: 'monster-0',
+        pos: {
+            x: -38.7,
+            y: -1.5,
+            z: 31
+        }
+    },
+    {
+        name: 'monster-1',
+        pos: {
+            x: 36.3,
+            y: -1.5,
+            z: -4.8
+        }
+    },
+    {
+        name: 'monster-2',
+        pos: {
+            x: -32.6,
+            y: -1.5,
+            z: -39.6    
+        }
+     },
+    {
+        name: 'monster-3',
+        pos: {
+            x: 25.2,
+            y: -1.5,
+            z: -55.6    
+        }
+    }
+]
--- src/game-object-collision.js
+++
@@ -1,7 +0,0 @@
-this.el.addEventListener('collide', function (e) {
-    console.log('Player has collided with ', e.detail.body.el);
-    e.detail.target.el; // Original entity (camera).
-    e.detail.body.el; // Other entity, which the camera touched.
-    e.detail.contact; // Stats about the collision (CANNON.ContactEquation).
-    e.detail.contact.ni; // Normal (direction) of the collision (CANNON.Vec3).
-});
